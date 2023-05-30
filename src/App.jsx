fc51cea84fc52b3c9cf695911d1bd76b6581f9fb
Gage Norton <nortongage0@gmail.com>
2023-05-30 15:41:10 -0500

    Add sections to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import About from "./sections/about/About"
+import Contact from "./sections/contact/Contact"
+import FloatingNav from "./sections/floating-nav/FloatingNav"
+import Footer from "./sections/footer/Footer"
+import Header from "./sections/header/Header"
+import Navbar from "./sections/navbar/Navbar"
+import Portfolio from "./sections/portfolio/Portfolio"
+
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+        <Navbar/>
+        <Header/>
+        <About/>
+        <Portfolio/>
+        <Contact/>
+        <Footer/>
+        <FloatingNav/>
+    </main>
   )
 }
 
