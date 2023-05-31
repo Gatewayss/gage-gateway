72a9bedc274a4140ffcf2fcbfdf4bf81090a015f
Gage Norton <nortongage0@gmail.com>
2023-05-31 11:25:12 -0500

    Add navbar structure

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
+import data from './data.js'
 import './navbar.css'
+import {IoIosColorPalette} from 'react-icons/io'
 
 export const Navbar = () => {
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className="container nav-container">
+        <a href='index.html'><h1>Gage Norton</h1></a>
+        <ul className="nav-menu">
+          {
+            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+        </ul>
+        <button id="theme-icon"><IoIosColorPalette/></button>
+      </div>
+    </nav>
   )
 }
 
