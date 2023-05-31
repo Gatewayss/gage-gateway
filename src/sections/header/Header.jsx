5af92c862374705dfe7b496b35fb1e474abb63a9
Gage Norton <nortongage0@gmail.com>
2023-05-31 12:21:55 -0500

    Add header socials

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,4 +1,5 @@
 import './header.css'
+import data from './data.js'
 import HeaderImage from '../../assets/portrait.jpg'
 
 const Header = () => {
@@ -10,11 +11,16 @@ const Header = () => {
         </div>
         <h3>Gage Norton</h3>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione tenetur possimus quisquam illum esse? Cupiditate animi ea maiores. Quo dignissimos laudantium voluptatum totam, sed ducimus suscipit nobis hic maiores in.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum vero, dolore quos dicta, ea at odit aliquam iste iusto explicabo ducimus, vel voluptatum repudiandae nobis. Fugiat doloribus vel assumenda? Cupiditate!
         </p>
         <div className="header-cta">
           <a href='#contact'>Let's Talk</a>
           <a href='#portfolio'>My Work</a>
+        </div>
+        <div className="header-socials">
+          {
+            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>
+          )}
         </div>
       </div>
     </header>
---
+++ src/sections/header/data.js
@@ -0,0 +1,17 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+
+const data = [
+    {
+        id: 1, link: 'https://www.instagram.com/gateway___/', icon: <AiOutlineInstagram/>
+    },
+    {
+        id: 2, link: 'https://www.linkedin.com/in/gage-norton-a3568725b/', icon: <AiOutlineLinkedin/>
+    },
+    {
+        id: 3, link: 'https://github.com/Gatewayss', icon: <AiFillGithub/>
+    }
+]
+
+export default data
