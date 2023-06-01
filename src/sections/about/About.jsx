8bff4a4864ad12161c7e2e1e43b98150439aec7b
Gage Norton <nortongage0@gmail.com>
2023-06-01 12:06:09 -0500

    Add about me cards

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -2,6 +2,8 @@ import './about.css'
 import AboutImg from '../../assets/portrait.jpg'
 import {HiDownload} from 'react-icons/hi'
 import Resume from '../../assets/Resume.pdf'
+import Card from '../../components/Card'
+import data from './data.js'
 
 const About = () => {
   return (
@@ -15,6 +17,16 @@ const About = () => {
         <div className="about-right">
           <h2>About Me</h2>
           <div className="about-cards">
+            {
+              data.map(card => (
+                <Card key={card.id} className="about-card">
+                  <span className='about-card-icon'>{card.icon}</span>
+                  <h5>{card.title}</h5>
+                  <small>{card.desc}</small>
+                </Card>
+              ))
+            }
+          </div>
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis labore et corporis, tempora quibusdam voluptatum optio minus officia minima eveniet quidem quaerat aperiam accusamus deleniti ratione a maiores inventore ipsum?
               </p>
@@ -22,7 +34,6 @@ const About = () => {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur odio quia, soluta ipsum esse natus. Doloribus possimus aut sit, nisi porro veniam iure reprehenderit tenetur quibusdam. Ab, voluptatibus eum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore magnam quam error repellat neque magni reiciendis dolores libero nisi, ullam voluptatem ex corrupti, quibusdam soluta, suscipit eos ut earum nobis.
               </p>
               <a href={Resume} className="btn primary" download>Download Resume <HiDownload/></a>
-          </div>
         </div>
       </div>
     </section>
---
+++ src/sections/about/data.js
@@ -0,0 +1,17 @@
+import {FaAward} from 'react-icons/fa'
+import {TbBooks} from 'react-icons/tb'
+import {BiHappyHeartEyes} from 'react-icons/bi'
+
+const data = [
+    {
+        id: 1, icon: <FaAward/>, title: 'Education', desc: 'UofM Fullstack Graduate'
+    },
+    {
+        id: 2, icon: <TbBooks/>, title: 'Projects', desc: '21+ completed'
+    }, 
+    {
+        id: 3, icon: <BiHappyHeartEyes/>, title: 'Skills', desc: '70+ tech skills'
+    }
+]
+
+export default data;
