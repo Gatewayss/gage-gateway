15c743bb0d17056b913cae1847ad72c6f33b2f18
Gage Norton <nortongage0@gmail.com>
2023-06-07 12:49:36 -0500

    Add footer content

---
+++ src/sections/footer/data.js
@@ -0,0 +1,19 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+
+export const links = [
+    {id: 1, link: '#', title: 'Home'},
+    {id: 2, link: '#about', title: 'About'},
+    {id: 3, link: '#services', title: 'Services'},
+    {id: 4, link: '#portfolio', title: 'Portfolio'},
+    {id: 5, link: '#contact', title: 'Contact'}
+]
+
+
+export const socials = [
+    {id: 1, link: 'https://www.instagram.com/gateway___/', icon: <AiOutlineInstagram/>},
+    {id: 2, link: 'https://www.linkedin.com/in/gage-norton-a3568725b/', icon: <AiOutlineLinkedin/>},
+    {id: 3, link: 'https://github.com/Gatewayss', icon: <AiFillGithub/>}
+]
