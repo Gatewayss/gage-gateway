5697656de1fd8806b9b40778eac6fa5958a9099c
Gage Norton <nortongage0@gmail.com>
2023-06-13 18:10:39 -0500

    Display projects

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,8 +1,20 @@
 import './portfolio.css'
+import data from './data'
+import Projects from './Projects'
+import { useState } from 'react'
 
 const Portfolio = () => {
+  const [projects, setProjects] = useState(data);
+
   return (
-    <section id="portfolio"><h2>Portfolio</h2></section>
+    <section id="portfolio">
+    <h2>Recent Projects</h2>
+    <p>
+    Check out some of the projects I recently worked on during my education at this U of M fullstack bootcamp.
+    </p>
+    
+      <Projects projects={projects}/>
+  </section>
   )
 }
 
