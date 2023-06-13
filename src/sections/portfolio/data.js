5e87d2e7994f13c84979623ea3bfc270ff2d87aa
Gage Norton <nortongage0@gmail.com>
2023-06-13 17:29:59 -0500

    Add portfolio data

---
+++ src/sections/portfolio/data.js
@@ -0,0 +1,32 @@
+import Image1 from '../../assets/challenge.png'
+import Image2 from '../../assets/age-filter.gif'
+import Image3 from '../../assets/Recipes for disaster.gif'
+
+const data = [ 
+    {
+        id: 1,
+        image: Image1,
+        title: 'Mixer (artist block drawing site)',
+        desc: 'Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!',
+        demo: 'https://paint-mixer.herokuapp.com/',
+        github: 'https://github.com/Gatewayss/mixer'
+    },
+    {
+        id: 2,
+        image: Image2,
+        title: 'Retro-great (social media app for Gen X)',
+        desc: 'Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!',
+        demo: 'https://retro-great-v2.herokuapp.com/',
+        github: 'https://github.com/Gatewayss/retro-great'
+    }, 
+    {
+        id: 3,
+        image: Image3,
+        title: 'Test (test test test)',
+        desc: 'Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!',
+        demo: 'https://paint-mixer.herokuapp.com/',
+        github: 'https://github.com/Gatewayss/mixer'
+    }
+]
+
+export default data;
