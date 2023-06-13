ffa875a6105d1be57028332f7f86b0625127c050
Gage Norton <nortongage0@gmail.com>
2023-06-13 17:15:24 -0500

    Update contact info and icons

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,8 +1,19 @@
+import contacts from './data'
 import './contact.css'
 
 const Contact = () => {
   return (
-    <section id="section"><h2>Contact</h2></section>
+    <section id="contact">
+      <h2>Get In Touch</h2>
+      <p>
+        Send me a message using any of the links below!
+      </p>
+      <div className="container contact__container" data-aos="fade-up">
+        {
+          contacts.map(contact => <a key={contact.id} href={contact.link} target="_blank" rel="noopener noreferrer">{contact.icon}</a>)
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ src/sections/contact/data.js
@@ -0,0 +1,11 @@
+import {HiOutlineMail} from 'react-icons/hi'
+import {RiMessengerLine} from 'react-icons/ri'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+
+const data = [
+    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:gagenorton0@gmail.com'},
+    {id: 2, icon: <RiMessengerLine/>, link: 'https://www.messenger.com/t/100008155741570'},
+    {id: 3, icon: <AiOutlineLinkedin />, link: 'https://www.linkedin.com/in/gage-norton-a3568725b/'}
+]
+
+export default data
