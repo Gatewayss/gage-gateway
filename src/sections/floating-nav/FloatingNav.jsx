5856debf7980197ec3a1a445437c34a5591ae615
Gage Norton <nortongage0@gmail.com>
2023-06-14 15:54:48 -0500

    Add theme context

--- src/sections/floating-nav/FloatingNav.jsx
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -1,8 +1,17 @@
+import data from './data'
+import Scrollspy from 'react-scrollspy'
+import Nav from './Nav'
 import './floating-nav.css'
 
 const FloatingNav = () => {
   return (
-    <section id="floating-nav">FloatingNav</section>
+    <ul id="floating__nav">
+      <Scrollspy offset={-350} className='scrollspy' items={['header', 'about', 'services', 'portfolio', 'contact']} currentClassName="active">
+        {
+          data.map(item => <Nav key={item.id} item={item} />)
+        }
+      </Scrollspy>
+    </ul>
   )
 }
 
