00f79317c1b18aabf06e39d2fbeecde1e703cf3a
Gage Norton <nortongage0@gmail.com>
2023-06-20 16:04:28 -0500

    Change skill section text

--- src/sections/skills/data.js
+++ src/sections/skills/data.js
@@ -9,13 +9,13 @@ const data = [
         id: 1, icon: <SiAdobexd/>, title: 'UI/UX Design', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
     },
     {
-        id: 2, icon: <RiReactjsLine/>, title: 'Frontend Development', desc: "Your product will look good and will be accessible on all devices, including mobile phones, tablets, and desktop."
+        id: 2, icon: <RiReactjsLine/>, title: 'With expertise in frontend development, I will ensure that your product not only boasts an appealing visual design but also guarantees accessibility across all devices, encompassing mobile phones, tablets, and desktops.'
     },
     {
-        id: 3, icon: <FaServer/>, title: 'Backend Development', desc: "The security of your business/product is taken seriously right from the start of the project. I will make sure your website/app is secure from attacks."
+        id: 3, icon: <FaServer/>, title: 'Backend Development', desc: "I prioritize the security of your business/product right from the project's inception, ensuring that your website/app remains safeguarded against potential attacks."
     },
     {
-        id: 4, icon: <AiFillAppstore/>, title: 'App Development', desc: 'Passionate and eager to embrace emerging technologies, continuously expanding knowledge and keeping pace with the ever-evolving tech landscape and cutting-edge practices.'
+        id: 4, icon: <AiFillAppstore/>, title: 'App Development', desc: 'Passionate and eager to embrace emerging technologies, continuously expanding knowledge and keeping pace with the ever-evolving world of tech.'
     }
 ]
 
