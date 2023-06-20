fe881df757a6f154c72361c8b3aebef4172a6973
Gage Norton <nortongage0@gmail.com>
2023-06-20 16:06:50 -0500

    Fix skill error

--- src/sections/skills/data.js
+++ src/sections/skills/data.js
@@ -9,7 +9,7 @@ const data = [
         id: 1, icon: <SiAdobexd/>, title: 'UI/UX Design', desc: "My designs are modern and intuitive. I use industry-standard rules to make sure your users have fun using your product."
     },
     {
-        id: 2, icon: <RiReactjsLine/>, title: 'With expertise in frontend development, I will ensure that your product not only boasts an appealing visual design but also guarantees accessibility across all devices, encompassing mobile phones, tablets, and desktops.'
+        id: 2, icon: <RiReactjsLine/>, title: 'Frontend Development', desc: 'With expertise in frontend development, I will ensure that your product not only boasts an appealing visual design but also guarantees accessibility across all devices, encompassing mobile phones, tablets, and desktops.'
     },
     {
         id: 3, icon: <FaServer/>, title: 'Backend Development', desc: "I prioritize the security of your business/product right from the project's inception, ensuring that your website/app remains safeguarded against potential attacks."
