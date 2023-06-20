f55c8fb86cc13624e33b507d56fd72385d8cbcb0
Gage Norton <nortongage0@gmail.com>
2023-06-20 11:15:55 -0500

    Add theme change styles

--- src/theme/Theme.jsx
+++ src/theme/Theme.jsx
@@ -6,20 +6,20 @@ import './theme.css'
 
 const Theme = () => {
   return (
-    <Modal className="theme__modal">
+    <Modal className="theme-modal">
         <h3>Customize Your Theme</h3>
         <small>Change the primary and background color to your preference.</small>
-        <div className="theme__primary-wrapper">
+        <div className="theme-primary-wrapper">
             <h5>Primary Color</h5>
-            <div className="theme__primary-colors">
+            <div className="theme-primary-colors">
                 {
                     primaryColors.map(pColor => <PrimaryColor key={pColor.className} className={pColor.className}/>)
                 }
             </div>
         </div>
-        <div className="theme__background-wrapper">
+        <div className="theme-background-wrapper">
             <h5>Background Color</h5>
-            <div className="theme__background-colors">
+            <div className="theme-background-colors">
                 {
                     backgroundColors.map(bColor => <BackgroundColor key={bColor.className} className={bColor.className} />)
                 }
