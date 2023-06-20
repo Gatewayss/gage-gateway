2ce9fdc7ba8e716bf34d8af0472b0f716e7fb1d9
Gage Norton <nortongage0@gmail.com>
2023-06-20 15:29:23 -0500

    Change about me bio

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -28,10 +28,10 @@ const About = () => {
             }
           </div>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis labore et corporis, tempora quibusdam voluptatum optio minus officia minima eveniet quidem quaerat aperiam accusamus deleniti ratione a maiores inventore ipsum?
+              I am a recent graduate of U of M's full-stack web boot camp, pursuing my dream career. With almost ten years of experience in the food service industry, I was looking for a change that would require more flexible hours and a different set of problem-solving skills. Since discovering coding through friends in this field, I have developed a passion for web development. I am excited to continue developing my skills and exploring the ever-evolving world of web development.
               </p>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam tenetur odio quia, soluta ipsum esse natus. Doloribus possimus aut sit, nisi porro veniam iure reprehenderit tenetur quibusdam. Ab, voluptatibus eum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore magnam quam error repellat neque magni reiciendis dolores libero nisi, ullam voluptatem ex corrupti, quibusdam soluta, suscipit eos ut earum nobis.
+              When I'm not coding, I immerse myself in a world of diverse hobbies. Music, reading, and painting are my cherished pursuits. As much as I relish the joy of creating new things that others can use and enjoy, I am equally captivated by the thrill of solving complex problems. These passions fuel my drive and bring a sense of fulfillment to my life.
               </p>
               <a href={Resume} className="btn primary" download>Download Resume <HiDownload/></a>
         </div>
